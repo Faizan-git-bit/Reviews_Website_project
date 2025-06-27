cc9c4fafb08998cd0fcef706ce693fb2bf7222a0
Faizan-git-bit <qaiserf322@gmail.com>
2025-06-27 14:51:26 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+  <div class="footer-links">
+    <div class="footer-column">
+      <h3>Company</h3>
+      <ul>
+        <li><a href="about.php">About Us</a></li>
+        
+      </ul>
+    </div>
+    <div class="footer-column">
+      <h3>Support</h3>
+      <ul>
+        <li><a href="support.php" onclick="openSupportModal()">Customer Support</a></li>
+        <li><a href="#">Help Center</a></li>
+       
+      </ul>
+    </div>
+    <div class="footer-column">
+      <h3>Legal</h3>
+      <ul>
+        <li><a href="#">Terms of Service</a></li>
+       
+      </ul>
+       
+    </div>
+    <div class="footer-column">
+      
+      <ul>
+      <li><a href="logout.php">logout</a></li>
+       
+      </ul>
+       
+    </div>
+
+  </div>
+  <p class="footer-bottom">© 2025 ReviewSphere. All rights reserved.</p>
+</footer>
+
---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <a class="navbar-logo" href="#hero-section">ReviewSphere</a>
+    <ul class="navbar-links">
+       <li><a href="index.php">Home</a></li>
+      <li><a href="dashboard.php">Trustpilot reviews</a></li>
+      <li><a href="submit.php">Write a review</a></li>
+      <li><a href="category.php">Categories</a></li>
+      <li><a href="login.php">Log in</a></li>
+    </ul>
+  </nav>
